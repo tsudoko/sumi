44e83660d9bd9496f6dcd0d068c93b92fa40f6bb
flan <flan@flande.re>
2016-08-13 02:32:27 +0200

    switch to go-gtk
    It has a worse API, but it supports more features right now. Might
    consider going back to ui after some time.

--- main.go
+++ main.go
@@ -5,36 +5,21 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/andlabs/ui"
+	"github.com/mattn/go-gtk/gdk"
+	"github.com/mattn/go-gtk/glib"
+	"github.com/mattn/go-gtk/gtk"
 )
 
-type CboxFunc func(*ui.Combobox)
-
-type ValueCbox struct {
-	values         []rune
-	cbox           *ui.Combobox
-	CallOnSelected func(*ui.Combobox)
-}
-
-func (c *ValueCbox) Selected() rune {
-	return c.values[c.cbox.Selected()]
-}
-
-func NewValueCbox(matches []rune) *ValueCbox {
-	vcb := &ValueCbox{matches, ui.NewCombobox(), nil}
-	for _, m := range matches {
-		vcb.cbox.Append(string(m))
-	}
-	vcb.cbox.SetSelected(0)
-	return vcb
-}
-
-func generateBoxes(matches [][]rune) []*ValueCbox {
-	boxes := make([]*ValueCbox, 0, 3)
+func generateBoxes(matches [][]rune) []*gtk.ComboBoxText {
+	boxes := make([]*gtk.ComboBoxText, 0, 3)
 	for _, b := range matches {
-		boxes = append(boxes, NewValueCbox(b))
+		cb := gtk.NewComboBoxText()
+		for _, m := range b {
+			cb.AppendText(string(m))
+		}
+		cb.SetActive(0)
+		boxes = append(boxes, cb)
 	}
-
 	return boxes
 }
 
@@ -42,117 +27,115 @@ func cleanup(path string) {
 	os.RemoveAll(path)
 }
 
-func cbModifyEntry(e *ui.Entry, i int, v *ValueCbox) func(*ui.Combobox) {
-	return func(*ui.Combobox) {
-		old := e.Text()
+func cbModifyEntry(e *gtk.Entry, i int, cbt *gtk.ComboBoxText) func() {
+	return func() {
+		old := e.GetText()
 		runes := []rune(old)
-		oldLen := len(runes)
 
 		for j := len(runes); i >= len(runes); j++ {
 			runes = append(runes, '　')
 		}
 
-		runes[i] = v.Selected()
+		if cbt.GetActiveText() != "" {
+			runes[i] = []rune(cbt.GetActiveText())[0]
+		}
 		new_ := string(runes)
 		e.SetText(new_)
-
-		if i < oldLen {
-			fmt.Println(e.Text())
-		}
 	}
 }
 
-func cbSelectArea(w *ui.Window, g *ui.Group, entry *ui.Entry, tempDir string) func(*ui.Button) {
-	return func(button *ui.Button) {
+func cbSelectArea(w *gtk.Window, butt *gtk.Button, box *gtk.Box, entry *gtk.Entry, tempDir string) func() {
+	return func() {
 		var matches [][]rune
 
-		button.Disable()
+		butt.SetSensitive(false)
 		imgPath, err := TakeScreenshot(tempDir+string(os.PathSeparator)+"sumi", "")
 
 		if err != nil {
-			ui.MsgBoxError(w, "Error", err.Error())
-			button.Enable()
+			MsgBoxError(w, err.Error())
+			butt.SetSensitive(true)
 			return
 		}
 
-		label := ui.NewLabel("Detecting...")
+		DestroyAllChildren(&box.Container)
 
-		g.SetChild(label)
+		label := gtk.NewLabel("Detecting...")
+		box.Add(label)
+		label.Show()
 
 		go func() {
 			matches, err = detectCharacters(imgPath)
-			ui.QueueMain(func() {
-				label.SetText("")
-				button.Enable()
+			gdk.ThreadsEnter()
+			label.SetText("")
+			butt.SetSensitive(true)
 
-				if err != nil {
-					ui.MsgBoxError(w, "Error", err.Error())
-					return
-				}
+			if err != nil {
+				MsgBoxError(w, err.Error())
+				return
+			}
 
-				box := ui.NewHorizontalBox()
-				boxes := generateBoxes(matches)
+			boxes := generateBoxes(matches)
 
-				entry.SetText("")
-				for i, e := range boxes {
-					box.Append(e.cbox, true)
-					e.cbox.OnSelected(cbModifyEntry(entry, i, e))
-					e.CallOnSelected = cbModifyEntry(entry, i, e)
-					e.CallOnSelected(e.cbox)
-				}
+			DestroyAllChildren(&box.Container)
 
-				fmt.Println(entry.Text())
-				g.SetChild(box)
-			})
+			entry.SetText("")
+			for i, e := range boxes {
+				box.PackStart(e, true, true, 0)
+				e.Connect("changed", cbModifyEntry(entry, i, e))
+				e.Emit("changed")
+			}
+
+			box.ShowAll()
+			gdk.Flush()
+			gdk.ThreadsLeave()
 		}()
 	}
 }
 
 func MainWindow() {
-	selectButton := ui.NewButton("セレクト")
-	resultEntry := ui.NewEntry()
+	selectButton := gtk.NewButtonWithLabel("セレクト")
+	resultEntry := gtk.NewEntry()
 
-	matchesGroup := ui.NewGroup("")
+	mainbox := gtk.NewHBox(false, 0)
+	matchbox := gtk.NewHBox(false, 0)
+	otherbox := gtk.NewVBox(true, 0)
+	toolbar := gtk.NewVBox(true, 0)
 
-	mainbox := ui.NewHorizontalBox()
-	otherbox := ui.NewVerticalBox()
-	toolbar := ui.NewVerticalBox()
-
-	w := ui.NewWindow("すみ", 0, 0, false)
+	w := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	w.SetTitle("すみ")
 
 	tempDir, err := ioutil.TempDir("", "sumi")
 	if err != nil {
-		ui.MsgBoxError(w, "Error", err.Error())
+		MsgBoxError(w, err.Error())
 	}
 
-	toolbar.Append(selectButton, true)
-	otherbox.Append(resultEntry, true)
-	otherbox.Append(matchesGroup, false)
+	toolbar.PackStart(selectButton, true, true, 0)
+	otherbox.PackStart(resultEntry, true, true, 0)
+	otherbox.PackStart(matchbox, false, false, 0)
 	// do we really need dynamic box generation? most words are less than 3 chars long
-	selectButton.OnClicked(cbSelectArea(w, matchesGroup, resultEntry, tempDir))
-	resultEntry.OnChanged(func(e *ui.Entry) {
-		fmt.Println(e.Text())
+	selectButton.Connect("clicked", cbSelectArea(w, selectButton, &matchbox.Box, resultEntry, tempDir))
+	resultEntry.Connect("changed", func() {
+		fmt.Println(resultEntry.GetText())
 	})
 
-	matchesGroup.SetMargined(false)
+	mainbox.PackStart(toolbar, false, false, 0)
+	mainbox.PackStart(otherbox, true, true, 0)
 
-	mainbox.Append(toolbar, false)
-	mainbox.Append(otherbox, true)
-
-	w.SetChild(mainbox)
-	w.OnClosing(func(*ui.Window) bool {
-		ui.Quit()
+	w.Add(mainbox)
+	w.Connect("destroy", func() {
 		cleanup(tempDir)
-		return true
+		gtk.MainQuit()
 	})
 
-	w.Show()
+	w.ShowAll()
 }
 
 func main() {
-	err := ui.Main(MainWindow)
-
-	if err != nil {
-		panic(err)
-	}
+	glib.ThreadInit()
+	gdk.ThreadsInit()
+	gtk.Init(nil)
+	gdk.ThreadsEnter()
+	MainWindow()
+	gtk.Main()
+	gdk.ThreadsLeave()
 }
---
+++ util_gtk.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"unsafe"
+
+	"github.com/mattn/go-gtk/gtk"
+)
+
+func MsgBoxError(w *gtk.Window, msg string) {
+	d := gtk.NewMessageDialog(w, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, msg)
+	d.Run()
+	d.Destroy()
+}
+
+func DestroyAllChildren(c *gtk.Container) {
+	c.GetChildren().ForEach(func(p unsafe.Pointer, _ interface{}) {
+		child := gtk.WidgetFromNative(p)
+		child.Destroy()
+	})
+	c.GetChildren().Free()
+}
